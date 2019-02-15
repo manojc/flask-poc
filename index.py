78a1123b6f7f81a742a255cb26700b3403049472
Manoj Chalode <chalodem@gmail.com>
2019-02-15 12:51:47 +0000

    added rest API

--- index.py
+++ index.py
@@ -1,17 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
+
 
 @app.route("/")
 @app.route("/home")
 def hello():
-    return render_template("index.html");
+    return render_template("index.html")
+
 
 @app.route("/about-us")
 def aboutUs():
-    return render_template("about-us.html");
+    return render_template("about-us.html")
+
+
+@app.route("/api/budget/autotrader")
+def autotraderBudgetPredictor():
+    data = {
+        "value": 1
+    }
+    return jsonify(data)
 
 # default route - error page
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return render_template("error.html");
+    return render_template("error.html")
