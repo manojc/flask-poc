4926bfb88ce494c351798258c64bef661310fbae
Manoj Chalode <chalodem@gmail.com>
2019-02-20 16:48:12 +0000

    added autotrader model to calculate the budget.

--- src/app/ml/models/autotrader_budget_model.py
+++ src/app/ml/models/autotrader_budget_model.py
@@ -1,67 +1,34 @@
 def predict_budget(request):
+    if (int(request["total_car_price"]) >= 5872200 and int(request["car_count"]) >= 135219):
+        return 999
+    elif (int(request["total_car_price"]) > 3278076 and int(request["total_car_price"]) <= 5872200 and int(request["car_count"]) > 92111 and int(request["car_count"]) <= 135219):
+        return 999
+    elif (int(request["total_car_price"]) > 2287621 and int(request["total_car_price"]) <= 328076 and int(request["car_count"]) > 68015 and int(request["car_count"]) <= 92111):
+        return 899
+    elif (int(request["total_car_price"]) > 1552400 and int(request["total_car_price"]) <= 2287621 and int(request["car_count"]) > 50139 and int(request["car_count"]) <= 68015):
+        return 799
+    elif (int(request["total_car_price"]) > 907690 and int(request["total_car_price"]) <= 1552400 and int(request["car_count"]) > 32067 and int(request["car_count"]) <= 50139):
+        return 699
+    elif (int(request["total_car_price"]) > 350017 and int(request["total_car_price"]) <= 907690 and int(request["car_count"]) > 16517 and int(request["car_count"]) <= 32067):
+        return 599
+    elif (int(request["total_car_price"]) > 150294 and int(request["total_car_price"]) <= 350017 and int(request["car_count"]) > 8931 and int(request["car_count"]) <= 16517):
+        return 499
+    elif (int(request["total_car_price"]) > 69676 and int(request["total_car_price"]) <= 150294 and int(request["car_count"]) > 4586 and int(request["car_count"]) <= 8931):
+        return 399
+    elif (int(request["total_car_price"]) > 28108 and int(request["total_car_price"]) <= 69676 and int(request["car_count"]) > 2224 and int(request["car_count"]) <= 4586):
+        return 299
+    elif (int(request["total_car_price"]) > 4651 and int(request["total_car_price"]) <= 28108 and int(request["car_count"]) > 322 and int(request["car_count"]) <= 2224):
+        return 199
+    elif (int(request["total_car_price"]) > 0 and int(request["total_car_price"]) <= 4651 and int(request["car_count"]) > 0 and int(request["car_count"]) <= 322):
+        return 99
+    else:
+        return None
 
-    print("This is autotrader budget predictor")
 
-    print(request["dealer_id"])
-    print(request["total_car_price"])
-    print(request["average_car_price"])
-    print(request["car_count"])
-    print(request["currency"])
-
-    return {
-        "data": {
-            "currency": "USD",
-            "predicted_budget": [
-                {
-                    "term": "annually",
-                    "budget": 2000,
-                    "recommended": False
-                },
-                {
-                    "term": "monthly",
-                    "budget": 200,
-                    "recommended": True
-                },
-                {
-                    "term": "weekly",
-                    "budget": 60,
-                    "recommended": False
-                },
-                {
-                    "term": "daily",
-                    "budget": 10,
-                    "recommended": False
-                }
-            ],
-            "budget_windows": [
-                {
-                    "id": 1,
-                    "budget": 100,
-                    "term": "monthly"
-                },
-                {
-                    "id": 2,
-                    "budget": 200,
-                    "term": "monthly"
-                },
-                {
-                    "id": 3,
-                    "budget": 300,
-                    "term": "monthly"
-                },
-                {
-                    "id": 4,
-                    "budget": 400,
-                    "term": "monthly"
-                },
-                {
-                    "id": 5,
-                    "budget": 500,
-                    "term": "monthly"
-                }
-            ]
-        },
-        "code": "200",
-        "message": None,
-        "is_successful": True
-    }
+# call the function
+# ================-
+if __name__ == '__main__':
+    predict_budget({
+        "total_car_price": "200000",
+        "car_count": 60
+    })
