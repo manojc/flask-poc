7ab11abe3755865e0d46efd74a2f5a5a788e6c16
Manoj Chalode <chalodem@gmail.com>
2019-02-20 16:15:00 +0000

    added adwords api

--- src/app/api/impressions/impressions_api.py
+++ src/app/api/impressions/impressions_api.py
@@ -1,60 +1,13 @@
 from flask import Blueprint, jsonify, request
+from app.googleads.authenticate import main as authenticate
+from app.googleads.estimate_keyword_traffic import main as estimator
 
 impressions_mod = Blueprint('impressions_api', __name__)
 
 
 @impressions_mod.route("/impressions", methods=['POST'])
 def autotraderBudgetPredictor():
-    print(request.get_json())
-    data = {
-        "data": {
-            "selected_location": "new York",
-            "selected_cpm": 1.2,
-            "selected_currency": "USD",
-            "selected_keywords": [
-                "ford"
-            ],
-            "selected_budget": {
-                "term": "monthly",
-                "budget": 200,
-                "recommended": True
-            },
-            "selected_total_car_price": "200000",
-            "selected_average_car_price": "4000",
-            "selected_car_count": 60,
-            "predictions": [
-                {
-                    "radius": {
-                        "value": 2,
-                        "unit": "KM"
-                    },
-                    "budget": 99,
-                    "impressions": 100000,
-                    "clicks": 2000,
-                    "marketing_predefined_audiences": None,
-                    "leads": None,
-                    "number_of_sales": None,
-                    "sales_value": None,
-                    "recommended": True
-                },
-                {
-                    "radius": {
-                        "value": 5,
-                        "unit": "KM"
-                    },
-                    "budget": 199,
-                    "impressions": 200000,
-                    "clicks": 4000,
-                    "marketing_predefined_audiences": None,
-                    "leads": None,
-                    "number_of_sales": None,
-                    "sales_value": None,
-                    "recommended": False
-                }
-            ]
-        },
-        "code": "200",
-        "message": None,
-        "is_successful": True
-    }
-    return jsonify(data)
+    keyword_request = request.get_json()
+    client = authenticate()
+    data = estimator(client, keyword_request)
+    return data
---
+++ src/app/googleads/authenticate.py
@@ -0,0 +1,43 @@
+from googleads import adwords
+from googleads import oauth2
+
+# OAuth2 credential information. In a real application, you'd probably be
+# pulling these values from a credential storage.
+CLIENT_ID = '187908472532-4cjp0rfl32916i1vbjv7c8culteniipt.apps.googleusercontent.com'
+CLIENT_SECRET = 'AR_DNTd8ylG5XzTKKiEcxRDY'
+REFRESH_TOKEN = '1/hyZTbS4LlcFKoWQ_XvAe8Ps9K4nNQ7I7lQYLlfMvxmk'
+
+# AdWords API information.
+DEVELOPER_TOKEN = 'womxUeF2tS3RJ0-ckxNmTw'
+USER_AGENT = 'INSERT_USER_AGENT_HERE'
+CLIENT_CUSTOMER_ID = '410-224-8876'
+
+
+def main():
+    client_id = CLIENT_ID
+    client_secret = CLIENT_SECRET
+    refresh_token = REFRESH_TOKEN
+    developer_token = DEVELOPER_TOKEN
+    user_agent = USER_AGENT
+    client_customer_id = CLIENT_CUSTOMER_ID
+    oauth2_client = oauth2.GoogleRefreshTokenClient(
+        client_id, client_secret, refresh_token)
+
+    adwords_client = adwords.AdWordsClient(
+        developer_token, oauth2_client, user_agent,
+        client_customer_id=client_customer_id)
+
+    customer_service = adwords_client.GetService(
+        'CustomerService', version='v201809')
+    customers = customer_service.getCustomers()
+
+    print('You are logged in as a user with access to the following customers:')
+
+    for customer in customers:
+        print('\t%s' % customer['customerId'])
+
+    return adwords_client
+
+
+if __name__ == '__main__':
+    main()
